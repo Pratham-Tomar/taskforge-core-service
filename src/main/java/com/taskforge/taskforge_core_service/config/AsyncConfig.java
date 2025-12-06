3d4261aaa79986876ab9485320323881deb47228
Pratham Tomar <prathamtomar02@gmail.com>
2025-12-06 21:53:26 +0530

    Added the AsyncConfig class for the Thread Managemenet

---
+++ src/main/java/com/taskforge/taskforge_core_service/config/AsyncConfig.java
@@ -0,0 +1,34 @@
+package com.taskforge.taskforge_core_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor(){
+        ThreadPoolTaskExecutor executor= new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("TaskForge-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = "fileProcessingExecutor")
+    public Executor fileProcessingExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("fileProcessingExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
